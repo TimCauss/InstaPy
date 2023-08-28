7000a9757c87d7f30f8a442c3f4756a9aa9d7851
TimCauss <t.caussignac@gmail.com>
2023-08-28 13:34:32 +0200

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+
+
+class Person:
+    def display(self):
+        print('I am a person', self)
+
+    def greet(self):
+        print('Hello, how are you doing ?', self)
+
+
+p1 = Person()
+p2 = Person()
+
+p1.display()
+p1.greet()
+
+p2.display()
+p2.greet()
